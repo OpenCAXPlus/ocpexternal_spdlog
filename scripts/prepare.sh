bfae7c283d8546c3ff0919d4398dbb75f68f5a7f
Xiaoxing Cheng <hello@chengxiaoxing.me>
2023-07-10 22:17:44 +0800

    update the prepare script, and ocp.yml scripts part

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -24,8 +24,10 @@ mkdir -p "$dir"
 echo "Create ocp folder structure"
 tar -xzf $gz_file -C $dir --strip-components=1
 echo "Extract files to ocp folder"
-cp -r config "$dir/.."
+cp -r configurations "$dir/.."
 echo "Copy all configurations to ocp folder"
+cp -r scripts "$dir/.."
+echo "Copy all scripts to ocp folder"
 cp ocp.yml "$dir/.."
 echo "Copy ocp.yml to ocp folder"
 
